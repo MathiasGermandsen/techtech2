8915eb871945df45f092779bc4f123f7792a3ca4
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-06-19 13:13:38 +0200

    changes

--- techtech2/obj/Debug/net8.0/techtech2.AssemblyInfo.cs
+++ techtech2/obj/Debug/net8.0/techtech2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("techtech2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5226faedc2764eedf7afc68d416ab4c868359c0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f9529bd39370678a78049a935fb9f29acfcee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("techtech2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("techtech2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
